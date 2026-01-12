3b8b7ec7debff87b548d53f3d2e2cf931f01f52a
Patrick Stünkel <webminz@googlemail.com>
2026-01-12 15:05:56 +0100

    added console entry point

--- src/frcm/__main__.py
+++ src/frcm/__main__.py
@@ -1,28 +1,9 @@
 import sys
 from pathlib import Path
+from frcm import console_main
 from frcm.datamodel.model import WeatherData
 from frcm.fireriskmodel.compute import compute
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print("Wrong number of arguments provided! Please provide one reference to a CSV file with weatherdata to compute the fire risk")
-        sys.exit(1)
-
-    file = Path(sys.argv[1])
-    wd = WeatherData.read_csv(file)
-
-    if len(wd.data) == 0:
-        print("Given file did not contain any data points! Please check the input format! Aborting...")
-        sys.exit(1)
-
-    print(f"Computing FireRisk for given data in '{file.absolute()}' ({len(wd.data)} datapoints)", end="\n\n")
-
-    risks = compute(wd)
-
-    if len(sys.argv) == 3:
-        output = Path(sys.argv[2])
-        risks.write_csv(output)
-        print(f"Calculated fire risks written to '{output.absolute()}'")
-    else:
-        print(risks)
-
+    console_main()
